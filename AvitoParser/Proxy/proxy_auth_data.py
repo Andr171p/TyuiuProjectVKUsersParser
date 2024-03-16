f97dc7971e1057e950fbebcdfdce67647a8f2def
Tyuiu.KosovAS@outlook.com <andrey.kosov.05@inbox.ru>
2024-03-16 20:43:34 +0500

    first

---
+++ AvitoParser/Proxy/proxy_auth_data.py
@@ -0,0 +1,11 @@
+# proxy login/password:
+proxy_login = "dB3PqQ"
+proxy_password = "dJVVEo"
+
+# proxy options:
+proxy_options = {
+    "proxy": {
+        "https": f"https://{proxy_login}:{proxy_password}@193.233.125.150:8000"
+    }
+}
+
